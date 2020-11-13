c17389636963467e9741a3555d9da23f155b0282
Stefano Gualandi <stefano.gualandi@gmail.com>
2020-11-13 22:44:15 +0100

    Fixed readme and test file

--- wrappers/python/test_kwd.py
+++ wrappers/python/test_kwd.py
@@ -8,22 +8,31 @@ from KWD import *
 
 # Define first histogram
 a = Histogram2D()
+# Add at position (0,0) a unit of mass
 a.add(0, 0, 1)
+# Normalize the histogram
 a.normalize()
 
 # Define second histogram
 b = Histogram2D()
-b.add(1, 1, 1)
+# Add at position (1,1) a unit of mass
+b.add(1, 1, 2)
+# Normalize the histogram
 b.normalize()
 
 # Define third histogram
 c = Histogram2D()
-c.add(1, 0, 1)
-c.add(0, 1, 1)
+# Add at position (1,0) and (0,1) an half unit of mass
+c.add(1, 0, 0.5)
+c.add(0, 1, 0.5)
+# Add at position (5,5) a unit of mass
 c.add(5, 5, 1)
+# Normalize the histogram
 c.normalize()
- 
-# Define a solver and compute the distance
+
+# Define a solver and compute the distance 
+# Kantorovich-Wasserstein distance of order 1
+# with L_2 as ground distance
 s = Solver()
 
 print("d(a,b) =", s.distance(a, b, 3))
