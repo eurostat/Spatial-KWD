99aeccc7951150597b325e06de5784bee70e3c8e
Stefano Gualandi <stefano.gualandi@gmail.com>
2020-11-26 18:55:46 +0100

    Added support for data.table

--- examples/test_SKWD_dataframe.R
+++ examples/test_SKWD_dataframe.R
@@ -14,10 +14,41 @@ y <- as.integer(runif(N, 0, 31))
 h1 <- runif(N, 0, 1)
 h2 <- runif(N, 0, 1)
 
-# Data frame
+# Data frame with four columns named: "x", "y", "h1", "h2"
 df <- data.frame(x, y, h1, h2)
 
 # Compute distance
+print("Test with data frame:")
 d <- distanceDF(df, 3)
-
 print(d)
+
+
+# Data table
+require(data.table)
+# The table needs to have at least the four columns: "x", "y", "h1", "h2"
+dt <- data.table(x=x, y=y, h1=h1, h2=h2)
+
+# Compute distance
+print("Test with data table:")
+d <- distanceDF(dt, 3)
+print(d)
+
+
+# Increasing the second value, it increase the precision of 
+# the computed distance, but it can slow down the runtime speed
+print("Benchmarking precision versus speed:")
+start_time <- Sys.time()
+d <- distanceDF(dt, 4)
+print(c(d, Sys.time()-start_time))
+
+start_time <- Sys.time()
+d <- distanceDF(dt, 5)
+print(c(d, Sys.time()-start_time))
+
+start_time <- Sys.time()
+d <- distanceDF(dt, 20)
+print(c(d, Sys.time()-start_time))
+
+start_time <- Sys.time()
+d <- distanceDF(dt, 30)
+print(c(d, Sys.time()-start_time))
